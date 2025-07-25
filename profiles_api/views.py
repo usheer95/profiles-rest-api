053b30c65e57b3a82f8bcae9d59a4d6c73d0ac86
Alastair Usher <usheer1995@gmail.com>
2025-07-25 08:44:09 +0100

    Add search filter to user profile api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, filters
 from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers, models, permissions
@@ -109,5 +109,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication, )
-    permission_classes = (permissions.UpdateOwnProfile, )    
+    permission_classes = (permissions.UpdateOwnProfile, )
+    filter_backends = (filters.SearchFilter, )    
+    search_fields = ('name', 'email', )
 
