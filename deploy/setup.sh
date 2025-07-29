bb5638e126df9221a7abfc55593fcd19bb84d88e
Alastair Usher <usheer1995@gmail.com>
2025-07-29 15:26:53 +0100

    Fix location of uv

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -14,7 +14,8 @@ locale-gen en_GB.UTF-8
 echo "Installing dependencies..."
 apt-get update
 apt-get install -y sqlite3 supervisor nginx git
-wget -O - https://astral.sh/uv/install.sh | env UV_INSTALL_DIR="usr/local/bin" sh
+wget -O - https://astral.sh/uv/install.sh | env UV_INSTALL_DIR="/usr/local/bin" sh
+source usr/local/bin/env
 
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
