7ed89d1eb9d3ca0734d6c50efd56b0ca9fcb4e07
Alastair Usher <usheer1995@gmail.com>
2025-07-29 16:40:12 +0100

    Fixed update scipt to collect static

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,7 +7,7 @@ PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 cd $PROJECT_BASE_PATH
 git pull
 uv run manage.py migrate
-uv run collectstatic --noinput
+uv run manage.py collectstatic --noinput
 supervisorctl restart profiles_api
 
 echo "DONE! :)"
